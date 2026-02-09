782777c22ab88c2129fe461db969e2fa6f4b35b4
Mai-Kizaki <031424@gmail.com>
2026-02-09 14:55:32 +0900

    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_016</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        class Food {
+            private $name, $price;
+
+            public function show_price() {
+                echo $this->price . '<br>';
+            }
+
+            public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+            }
+        }
+
+        $food = new Food('potato', 250);
+
+        print_r($food);
+
+        echo '<br>';
+        ?>
+    </p>
+    <p>
+        <?php
+        class Animal {
+            private $name, $height, $weight;
+
+            public function show_height() {
+                echo $this->height . '<br>';
+            }
+
+            public function __construct(string $name, int $height, int $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+        }
+
+        $animal = new Animal('dog', 60, 5000);
+
+        print_r($animal);
+
+        echo '<br>';
+
+        $food->show_price();
+        $animal->show_height();
+        ?>
+    </p>
+</body>
+
+</html>
