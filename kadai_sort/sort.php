2377ffc0da37a848e047bbea50dce6c5d697d88b
Mai-Kizaki <031424@gmail.com>
2026-02-09 10:09:43 +0900

    kadai_sortを再アップロード

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -11,18 +11,18 @@
         <?php
         $nums = [15, 4, 18, 23, 10];
 
-        function sort_2way(&$array, $order) {
-          if ($order === TRUE) {
-              echo '昇順にソートします。<br>';
-              sort($array);
-          } else {
-              echo '昇順にソートします。<br>';
-              rsort($array);
-          }
+        function sort_2way($array, $order) {
+            if ($order === TRUE) {
+                echo '昇順にソートします。<br>';
+                sort($array);
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($array);
+            }
 
-          foreach ($array as $value) {
-              echo $value . '<br>';
-          }
+            foreach ($array as $value) {
+                echo $value . '<br>';
+            }
         }
 
         sort_2way($nums, TRUE);
